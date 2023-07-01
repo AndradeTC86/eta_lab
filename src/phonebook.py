ef9e7873f471cb0acb169670a3b6058e726db7aa
Thiago Cesar <andrade-tc@uol.com.br>
2023-07-01 14:16:32 -0300

    TDD

--- src/phonebook.py
+++ src/phonebook.py
@@ -10,42 +10,30 @@ class Phonebook:
         :param number: number of person in string
         :return: 'Nome invalido' or 'Numero invalido' or 'Numero adicionado'
         """
-        if '#' in name:
-            return 'Nome invalido'
-        if '@' in name:
-            return 'Nme invalido'
-        if '!' in name:
-            return 'Nome invalido'
-        if '$' in name:
-            return 'Nome invalio'
-        if '%' in name:
+        if '#' in name or '@' in name or '!' in name or '$' in name or '%' in name:
             return 'Nome invalido'
 
-        if len(number) < 0:
-            return 'Numero invalid'
+        if len(number) == 0:
+            return 'Numero invalido'
 
         if name not in self.entries:
             self.entries[name] = number
+            return 'Numero adicionado'
 
-        return 'Numero adicionado'
+        return 'Nome já existe'
 
     def lookup(self, name):
         """
         :param name: name of person in string
         :return: return number of person with name
         """
-        if '#' in name:
-            return 'Nome invaldo'
-        if '@' in name:
-            return 'Nome invalido'
-        if '!' in name:
-            return 'Nme invalido'
-        if '$' in name:
-            return 'Nome invalido'
-        if '%' in name:
-            return 'Nome nvalido'
 
-        return self.entries[name]
+        if name in self.entries:
+            return self.entries[name]
+        else:
+            if '#' in name or '@' in name or '!' in name or '$' in name or '%' in name:
+                return 'Nome invalido'
+        return 'Nome não encontrado'
 
     def get_names(self):
         """
@@ -77,7 +65,7 @@ class Phonebook:
         """
         result = []
         for name, number in self.entries.items():
-            if search_name not in name:
+            if search_name in name:
                 result.append({name, number})
         return result
 
@@ -86,14 +74,14 @@ class Phonebook:
 
         :return: return phonebook in sorted order
         """
-        return self.entries
+        return sorted(self.entries.items())
 
     def get_phonebook_reverse(self):
         """
 
         :return: return phonebook in reverse sorted order
         """
-        return self.entries
+        return sorted(self.entries.items(), reverse=True)
 
     def delete(self, name):
         """
@@ -101,5 +89,25 @@ class Phonebook:
         :param name: String with name
         :return: return 'Numero deletado'
         """
-        self.entries.pop(name)
-        return 'Numero deletado'
+
+        if name in self.entries:
+            self.entries.pop(name)
+            return 'Numero deletado'
+        return 'Numero não existe'
+
+    def get_name_by_number(self, number):
+        """
+        :param name: name of person in string
+        :return: return number of person with name
+        """
+
+        if number not in self.entries.values():
+            return 'Numero não encontrado'
+        return list(self.entries.keys())
+
+    def update(self, name, number):
+
+        if name in self.entries:
+            self.entries[name] = number
+            return 'Numero alterado'
+        return 'Nome não encontrado'
--- test/test_phonebook.py
+++ test/test_phonebook.py
@@ -1,4 +1,165 @@
+from src.phonebook import Phonebook
+
 class TestPhonebook:
 
-    def test_add(self):
-        assert False
+    def test_add_invalid_name(self):
+        name = 'Thi@go'
+        number = '12345678'
+        resultado_esperado = 'Nome invalido'
+        phonebook = Phonebook()
+        resultado = phonebook.add(name=name, number=number)
+        assert resultado_esperado == resultado
+
+    def test_add_invalid_number(self):
+        name = 'Thiago'
+        number = ''
+        resultado_esperado = 'Numero invalido'
+        phonebook = Phonebook()
+        resultado = phonebook.add(name=name, number=number)
+        assert resultado_esperado == resultado
+
+    def test_add_existing_contact(self):
+        name = 'POLICIA'
+        number = '190'
+        resultado_esperado = 'Nome já existe'
+        phonebook = Phonebook()
+        resultado = phonebook.add(name=name, number=number)
+        assert resultado_esperado == resultado
+
+    def test_add_number(self):
+        name = 'Thiago'
+        number = '12345678'
+        resultado_esperado = 'Numero adicionado'
+        phonebook = Phonebook()
+        resultado = phonebook.add(name=name, number=number)
+        assert resultado_esperado == resultado
+
+    def test_lookup_invalid_name(self):
+        name = 'T#iago'
+        resultado_esperado = 'Nome invalido'
+        phonebook = Phonebook()
+        resultado = phonebook.lookup(name=name)
+        assert resultado_esperado == resultado
+
+    def test_lookup_inexistent_name(self):
+        name = 'SAMU'
+        resultado_esperado = 'Nome não encontrado'
+        phonebook = Phonebook()
+        resultado = phonebook.lookup(name=name)
+        assert resultado_esperado == resultado
+
+    def test_lookup_name(self):
+        name = 'POLICIA'
+        resultado_esperado = '190'
+        phonebook = Phonebook()
+        resultado = phonebook.lookup(name=name)
+        assert resultado_esperado == resultado
+
+    def test_get_names(self):
+        resultado_esperado = ['POLICIA']
+        phonebook = Phonebook()
+        resultado = phonebook.get_names()
+        assert resultado_esperado == (list(resultado))
+
+    def test_get_number(self):
+        resultado_esperado = ['190']
+        phonebook = Phonebook()
+        resultado = phonebook.get_numbers()
+        assert resultado_esperado == (list(resultado))
+
+    def test_clear(self):
+        resultado_esperado = 'phonebook limpado'
+        phonebook = Phonebook()
+        resultado = phonebook.clear()
+        assert resultado_esperado == resultado
+
+    def test_search_name(self):
+        name = 'POL'
+        resultado_esperado = [{'190', 'POLICIA'}]
+        phonebook = Phonebook()
+        resultado = phonebook.search(search_name=name)
+        assert resultado_esperado == resultado
+
+    def test_invalid_search_name(self):
+        name = 'SAM'
+        resultado_esperado = []
+        phonebook = Phonebook()
+        resultado = phonebook.search(search_name=name)
+        assert resultado_esperado == resultado
+
+    def test_sorted_telephone_list(self):
+        resultado_esperado = [('BOMBEIRO', '193'), ('MEDICO', '192'), ('POLICIA', '190')]
+        name = 'BOMBEIRO'
+        number = '193'
+        name2 = 'MEDICO'
+        number2 = '192'
+        phonebook = Phonebook()
+        phonebook.add(name=name, number=number)
+        phonebook.add(name=name2, number=number2)
+        resultado = phonebook.get_phonebook_sorted()
+        assert resultado_esperado == resultado
+
+    def test_reversed_telephone_list(self):
+        resultado_esperado = [('POLICIA', '190'), ('MEDICO', '192'), ('BOMBEIRO', '193')]
+        name = 'BOMBEIRO'
+        number = '193'
+        name2 = 'MEDICO'
+        number2 = '192'
+        phonebook = Phonebook()
+        phonebook.add(name=name, number=number)
+        phonebook.add(name=name2, number=number2)
+        resultado = phonebook.get_phonebook_reverse()
+        assert resultado_esperado == resultado
+
+    def test_delete_name(self):
+        name = 'POLICIA'
+        resultado_esperado = 'Numero deletado'
+        phonebook = Phonebook()
+        resultado = phonebook.delete(name=name)
+        assert resultado_esperado == resultado
+
+    def test_delete_invalid_name(self):
+        name = 'SAMU'
+        resultado_esperado = 'Numero não existe'
+        phonebook = Phonebook()
+        resultado = phonebook.delete(name=name)
+        assert resultado_esperado == resultado
+
+    def test_get_name_by_number(self):
+        number = '190'
+        resultado_esperado = ['POLICIA']
+        phonebook = Phonebook()
+        resultado = phonebook.get_name_by_number(number=number)
+        assert resultado_esperado == resultado
+
+    def test_get_invalid_name_by_number(self):
+        number = '200'
+        resultado_esperado = 'Numero não encontrado'
+        phonebook = Phonebook()
+        resultado = phonebook.get_name_by_number(number=number)
+        assert resultado_esperado == resultado
+
+    def test_update_number(self):
+        name = 'POLICIA'
+        number = '192'
+        resultado_esperado = 'Numero alterado'
+        phonebook = Phonebook()
+        resultado = phonebook.update(name=name, number=number)
+        assert resultado_esperado == resultado
+
+    def test_persist_update_number(self):
+        name = 'POLICIA'
+        number = '192'
+        resultado_esperado = ['192']
+        phonebook = Phonebook()
+        phonebook.update(name=name, number=number)
+        resultado = phonebook.get_numbers()
+        assert resultado_esperado == (list(resultado))
+
+    def test_update_invalid_number(self):
+        name = 'SAMU'
+        number = '193'
+        resultado_esperado = 'Nome não encontrado'
+        phonebook = Phonebook()
+        resultado = phonebook.update(name=name, number=number)
+        assert resultado_esperado == resultado
